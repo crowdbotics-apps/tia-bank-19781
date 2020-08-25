e01796677c630803a42cc60a15da5e61d5bf18f7
Crowdbotics <team@crowdbotics.com>
2020-08-25 17:31:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tia_bank_19781/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tia_bank_19781;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "tia_bank_19781";
+    }
+}
---
+++ ios/tia_bank_19781Tests/tia_bank_19781Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface tia_bank_19781 : XCTestCase
+
+@end
+
+@implementation tia_bank_19781
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
